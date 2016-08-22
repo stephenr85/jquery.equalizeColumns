2e8051c8701019f88af336f4fd6f40d281725e93
Stephen Rushing <stephenr85@gmail.com>
2016-08-22 11:10:18 -0400

    Fixed bug when there are conflicting jQuery references on the page

--- jquery.equalizeColumns.js
+++ jquery.equalizeColumns.js
@@ -1,36 +1,39 @@
+(function($){
 
-jQuery.fn.equalizeColumns = function(selectors, qualifier){
-  var $ = jQuery;
-  if(selectors && !$.isArray(selectors)) selectors = [selectors];
-  
-  qualifier || (qualifier = function($item, $row){
-     return $item.position().top == $row.last().position().top;
-  });
-  
-  var I = this;
-  
-  //Find rows by top
-  var rows = [$()];
-  this.each(function(i, item){
-    var $item = $(item),
-        $row = rows[rows.length-1];
-    if(!$row.length || qualifier.call(I, $item, $row)){
-      rows[rows.length-1] = $row.add(item);
-    }else{
-      rows.push($item);
-    }
-  });
-  
-  for(var r = 0; r < rows.length; r++){
-    var $row = rows[r];
-    if($.isArray(selectors)){
-      for(var s = 0; s < selectors.length; s++){
-        $row.find(selectors[s]).equalizeBottoms(); 
-      }
-    }else{
-       $row.equalizeBottoms();
-    }
-  }
-  
-  return this;
-};
+	$.fn.equalizeColumns = function(selectors, qualifier){
+
+		if(selectors && !$.isArray(selectors)) selectors = [selectors];
+		
+		qualifier || (qualifier = function($item, $row){
+			 return $item.position().top == $row.last().position().top;
+		});
+		
+		var I = this;
+		
+		//Find rows by top
+		var rows = [$()];
+		this.each(function(i, item){
+			var $item = $(item),
+					$row = rows[rows.length-1];
+			if(!$row.length || qualifier.call(I, $item, $row)){
+				rows[rows.length-1] = $row.add(item);
+			}else{
+				rows.push($item);
+			}
+		});
+		
+		for(var r = 0; r < rows.length; r++){
+			var $row = rows[r];
+			if($.isArray(selectors)){
+				for(var s = 0; s < selectors.length; s++){
+					$row.find(selectors[s]).equalizeBottoms(); 
+				}
+			}else{
+				 $row.equalizeBottoms();
+			}
+		}
+		
+		return this;
+	};
+
+})(jQuery);
